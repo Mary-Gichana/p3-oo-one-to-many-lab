483155ae6925e9cb9f9a84f3413a604d89850022
Mary Monchari Gichana <marygichana95@gmail.com>
2024-12-05 22:08:15 +0300

    complete lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,44 @@
-class Pet:
-    pass
-
 class Owner:
-    pass
+    def __init__(self, name):
+        
+        if not isinstance(name, str):
+            raise Exception("Name must be a string.")
+        self.name = name
+        self._pets = [] 
+
+    def pets(self):
+    
+        return self._pets
+
+    def add_pet(self, pet):
+        
+        if not isinstance(pet, Pet):
+            raise Exception("Only instances of the Pet class can be added.")
+        pet.owner = self  
+        self._pets.append(pet)  
+
+    def get_sorted_pets(self):
+    
+        return sorted(self._pets, key=lambda pet: pet.name)
+
+
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = [] 
+
+    def __init__(self, name, pet_type, owner=None):
+        
+        if not isinstance(name, str):
+            raise Exception("Pet name must be a string.")
+        if pet_type not in self.PET_TYPES:
+            raise Exception(f"Invalid pet type. Allowed types: {', '.join(self.PET_TYPES)}.")
+        if owner and not isinstance(owner, Owner):
+            raise Exception("Owner must be an instance of the Owner class.")
+        
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+
+        if owner:
+            owner.add_pet(self) 
+        Pet.all.append(self)  
